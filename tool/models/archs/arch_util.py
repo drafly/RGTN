10e3c8b7078103e1fc9d4e824fc32e6b2e699580
converT98 <91445981+converT98@users.noreply.github.com>
2023-09-05 11:30:10 +0800

    Update arch_util.py

--- tool/models/archs/arch_util.py
+++ tool/models/archs/arch_util.py
@@ -7,13 +7,6 @@ from torch.nn.modules.batchnorm import _BatchNorm
 
 from tool.utils import get_root_logger
 
-# try:
-#     from tool.models.ops.dcn import (ModulatedDeformConvPack,
-#                                         modulated_deform_conv)
-# except ImportError:
-#     # print('Cannot import dcn. Ignore this warning if dcn is not used. '
-#     #       'Otherwise install tool with compiling dcn.')
-#
 
 @torch.no_grad()
 def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
